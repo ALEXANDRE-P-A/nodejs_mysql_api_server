c41985c3f3cff4af7005f7fb5a9c25345f8f364c
Alexandre <alexandrepereiraassano@gmail.com>
2024-05-20 22:13:35 +0900

    app completed ...

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "build")));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://nodejs-mysql-api-server-89c2f3154f98.herokuapp.com/');
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Methods', 'PUT, PATCH, DELETE, OPTIONS');
   next();
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 /* ----- MySQL DB Connection (ends here) ----- */
 
 app.get("/", (req, res) => {
-  res.send("NodeJS MySQL API Sever ...");
+  res.sendFile("/index.html");
 });
 
 app.get("/todos", async (req, res) => {
--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
   HOST: process.env.NODE_ENV === "global" ? process.env.MYSQL_HOST : process.env.LOCAL_HOST,
   PORT: process.env.NODE_ENV === "global" ? process.env.MYSQL_PORT : process.env.LOCAL_PORT,
   USERNAME: process.env.NODE_ENV === "global" ? process.env.MYSQL_USERNAME : process.env.LOCAL_USER,
   PASSWORD: process.env.NODE_ENV === "global" ? process.env.MYSQL_PASSWORD : process.env.LOCAL_PASS,
   DATABASE: process.env.NODE_ENV === "global" ? process.env.MYSQL_DATABASE : process.env.LOCAL_DBS,
-  CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
+  CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ? 
     parseInt(process.env.MYSQL_CONNECTION_LIMIT) : 10,
-  QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ?
+  QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ? 
     parseInt(process.env.MYSQL_QUEUE_LIMIT) : 0
 };
